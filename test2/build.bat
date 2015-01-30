c647401cc96f3d392aa28646c7b135fe4e51ad5e
utemkin <utemkin@inbox.ru>
2015-01-30 16:12:13 +0300

    build improved; fx2lp registers added

--- test2/build.bat
+++ test2/build.bat
@@ -1,2 +1,21 @@
-@if not exist build mkdir build
-@sdcc -mmcs51 --code-size 0x4000 --xram-loc 0x4000 --xram-size 0x0000 test.c -o build\
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+set BUILD_DIR=build
+set APP_NAME=test
+set CC=sdcc -c
+set CC_FLAGS=-mmcs51 --opt-code-speed --fomit-frame-pointer --model-small --code-size 0x4000 --iram-size 0x0100 --stack-size 0x0080 --xram-size 0x0000
+set LD=sdcc
+set LD_FLAGS=%CC_FLAGS%
+
+set SOURCES=test.c
+
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+
+for %%f in (%SOURCES%) do (
+  set OBJ=%BUILD_DIR%\%%~nf%.rel
+  set OBJS=!OBJS! !OBJ!
+  if exist !OBJ! del /q !OBJ!
+  %CC% %CC_FLAGS% %%f -o !OBJ!
+)
+%LD% %LD_FLAGS% %OBJS% -o %BUILD_DIR%\%APP_NAME%.hex
--- test2/compiler.h
+++ test2/compiler.h
@@ -5,10 +5,19 @@
 
 #if defined(SDCC)||defined(__SDCC)
 
+#define BIT __bit
+#define BYTE unsigned char
+#define WORD unsigned int
+#define DWORD unsigned long
+#define DATA __data
+#define XDATA __xdata
+#define IDATA __idata
+#define PDATA __pdata
+#define CODE __code
 #define DECLARE_SFR(name,addr) __sfr __at(addr) name
 #define DECLARE_SFR_BIT(name,addr,bit) __sbit __at(addr+bit) name
-#define DECLARE_XSFR(name,addr) __xdata volatile unsigned char __at(addr) name
-#define DECLARE_XSFR_ARRAY(name,addr,cnt) __xdata volatile unsigned char __at(addr) name[cnt]
+#define DECLARE_XSFR(name,addr) __xdata volatile BYTE __at(addr) name
+#define DECLARE_XSFR_ARRAY(name,addr,cnt) __xdata volatile BYTE __at(addr) name[cnt]
                                                          
 #else
 
