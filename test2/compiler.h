a4dffad541bc3492c3e5f7d91c7bda3c5e91f3ef
utemkin <utemkin@inbox.ru>
2015-01-30 19:38:58 +0300

    registers corrected (checked by comparing .map files with fx2lib)

--- test2/compiler.h
+++ test2/compiler.h
@@ -4,7 +4,12 @@
 #include "config.h"
 
 #if defined(SDCC)||defined(__SDCC)
+#define COMPILER_SDCC
+#else
+#error Unsupported compiler
+#endif
 
+#ifdef COMPILER_SDCC
 #define BIT __bit
 #define BYTE unsigned char
 #define WORD unsigned int
@@ -18,11 +23,6 @@
 #define DECLARE_SFR_BIT(name,addr,bit) __sbit __at(addr+bit) name
 #define DECLARE_XSFR(name,addr) __xdata volatile BYTE __at(addr) name
 #define DECLARE_XSFR_ARRAY(name,addr,cnt) __xdata volatile BYTE __at(addr) name[cnt]
-                                                         
-#else
-
-#error Unsupported compiler
-
 #endif
 
 #endif
