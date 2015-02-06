aff71fe11613420cc0d44041bc82fad34b38a481
utemkin <utemkin@inbox.ru>
2015-02-06 03:10:48 +0300

    handling of directories with spaces and so on improved

--- test2/build_sdcc.bat
+++ test2/build_sdcc.bat
@@ -1,21 +1,23 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
+setlocal ENABLEEXTENSIONS
 
 set BUILD_DIR=build_sdcc
 set APP_NAME=test
-set CC=sdcc -c
+set CC="sdcc" -c
 set CC_FLAGS=-mmcs51 --opt-code-speed --fomit-frame-pointer --model-small --code-size 0x4000 --iram-size 0x0100 --stack-size 0x0080 --xram-size 0x0000
-set LD=sdcc
+set LD="sdcc"
 set LD_FLAGS=%CC_FLAGS%
 
-set SOURCES=ivect.c test.c fx2lp.c
+set SOURCES=test.c fx2lp.c
 
-if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 
+set SOURCES=ivect.c %SOURCES%
 for %%f in (%SOURCES%) do (
-  set OBJ=%BUILD_DIR%\%%~nf%.rel
+  set OBJ="%BUILD_DIR%\%%~nf.rel"
   set OBJS=!OBJS! !OBJ!
   if exist !OBJ! del /q !OBJ!
   %CC% %CC_FLAGS% %%f -o !OBJ!
 )
-%LD% %LD_FLAGS% %OBJS% -o %BUILD_DIR%\%APP_NAME%.hex
+%LD% %LD_FLAGS% %OBJS% -o "%BUILD_DIR%\%APP_NAME%.hex"
